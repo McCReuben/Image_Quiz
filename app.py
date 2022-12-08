6f953c0838b2acf7be0dc01e8ba1bd251185b38b
McCReuben <thingkr@gmail.com>
2022-12-08 16:33:30 -0500

    File Skeltons

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
+import os
 from serpapi import GoogleSearch
-search = GoogleSearch({
-    "q": "coffee", 
-    "location": "Austin,Texas",
-    "api_key": "<your secret api key>"
-  })
-result = search.get_dict()
+
+API_key = os.getenv("SERP_API_KEY")
+API_key = "5074028568879874a7f573b4071fb9c484bf3e91e883b98d94949d6298b85f86"
+
+possible_search_terms = ["TEM","SEM","XRD","XRF","XANES"]
+
+def generate_microscope_search_params(microscope):
+    params = {
+        "q": "Images from a {}""".format(microscope),
+        "tbm": "isch",
+        "ijn": "0",
+        "api_key": API_key
+    }
+    return params
+
+search = GoogleSearch(generate_microscope_search_params(possible_search_terms[1]))
+results = search.get_dict()
+images_results = results["images_results"]
+
+
+print(images_results)
